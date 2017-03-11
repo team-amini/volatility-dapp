2032fe19aa9d95ff0664364a13a7142fe7f7fcd6
Bob Tiernay <bob.tiernay@okta.com>
2017-03-11 11:26:18 -0500

    Updated script to be so much cooler.

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#!/bin/bash 
+#
+# Script to spin up development environment with all dependencies satisfied
+#
+
+# Script to execute in container
 read -r -d '' script <<'EOF'
 echo '*** Starting testrpc'
 nohup testrpc --gasLimit 1111115141592 -g 1  -m volatility --account='0xafb293409a8c87be6ca3ca2ff1fca89af95e5dcf0bcb15125786b43a080de122,0x1000000000000000' > /tmp/testrpc.log 2>&1 &
@@ -19,7 +25,11 @@ echo '*** Starting app'
 npm start
 EOF
 
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-docker run -it -v $SCRIPTPATH:/app -p 3000:3000 -p 8545:8545 unpatent/docker-truffle /bin/bash -c "$script"
+# Configuration
+BASE_DIR=$(dirname $(realpath $0))
+APP_PORT=3000
+RPC_PORT=8545
+
+# Execute
+docker run -it -v $BASE_DIR:/app -p $APP_PORT:3000 -p $RPC_PORT:8545 unpatent/docker-truffle /bin/bash -e -c "$script"
 
